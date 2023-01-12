ae6fb4ccba70f3e27ec7bdd84b36571c7ff1ee1f
Layne <xhw@nihao.com>
2023-01-12 17:19:30 +0800

    second commit

--- src/main/java/com/atguigu/git/gitTest1.java
+++ src/main/java/com/atguigu/git/gitTest1.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class gitTest1 {
     public static void main(String[] args) {
         System.out.println("你好::第一次测试");
+        System.out.println("第二次测试");
     }
 }
