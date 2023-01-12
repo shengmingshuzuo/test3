3ce144e5af5cccddb398f1b3605eac8cfd8e796a
Layne <xhw@nihao.com>
2023-01-12 17:42:52 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/gitTest1.java
+++ src/main/java/com/atguigu/git/gitTest1.java
@@ -4,5 +4,6 @@ public class gitTest1 {
     public static void main(String[] args) {
         System.out.println("你好::第一次测试");
         System.out.println("第二次测试");
+        System.out.println("第三次了");
     }
 }
